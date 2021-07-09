633c4cee20c07593a899a47a14cf1a0175563239
xPolar <harismehrzad12@gmail.com>
2021-07-09 16:36:19 -0700

    Fix it spamming multiple times, if you have an issue with it reposting just increment the MS wait time.

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const request = require("request");
-const { writeFile } = require("fs");
 const { MessageEmbed, WebhookClient } = require("discord.js");
-const { lastPost, redditToScrape, webhookID, webhookToken } = require("./data.json");
+const { redditToScrape, webhookID, webhookToken } = require("./data.json");
+
+let lastPost = null;
+const webhookClient = new WebhookClient(webhookID, webhookToken);
 
 setInterval(() => {
     request(`https://reddit.com/r/${redditToScrape}/new.json?limit=1`, async (error, _, body) => {
         if (error) throw error;
 
         const data = JSON.parse(body).data.children[0].data;
-        if (lastPost === data.id || (data.link_flair_richtext[0]?.t?.toLowerCase() !== "hiring" && data.link_flair_richtext[0] !== undefined)) return;
-        const webhookClient = new WebhookClient(webhookID, webhookToken);
+        if (
+            lastPost === data.id ||
+            (data.link_flair_richtext[0]?.t?.toLowerCase() !== "hiring" &&
+                data.link_flair_richtext[0] !== undefined)
+        )
+            return;
         const embed = new MessageEmbed()
             .setAuthor(`u/${data.author}`, null, `https://reddit.com/u/${data.author}`)
             .setTitle(data.title)
@@ -17,21 +23,6 @@ setInterval(() => {
             .setDescription(data.selftext)
             .setColor("#FF3F18");
         await webhookClient.send(embed);
-        return writeFile(
-            "./data.json",
-            JSON.stringify(
-                {
-                    lastPost: data.id,
-                    redditToScrape: redditToScrape,
-                    webhookID: webhookID,
-                    webhookToken: webhookToken
-                },
-                null,
-                4
-            ),
-            (err) => {
-                if (err) throw err;
-            }
-        );
+        lastPost = data.id;
     });
-}, 2000);
+}, 3000);
